fe1f33a99c2f2ac7f1e1a0e7a12b2494dec1d154
Vlad Stirbu <vstirbu@gmail.com>
2024-04-30 09:10:03 +0300

    Add Kubernetes secret for user-vstirbu-gmail.com

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -33,7 +33,7 @@ export default async function Dashboard({
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link
-              href="/dashboard"
+              href="/account/dashboard"
               className="flex items-center gap-2 font-semibold"
             >
               <Rotate3D className="h-6 w-6" />
