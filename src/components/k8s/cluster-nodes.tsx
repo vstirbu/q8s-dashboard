34eb03eb5208613bff3225969beb5d4ee85f9879
Vlad Stirbu <vstirbu@gmail.com>
2024-05-17 16:05:45 +0300

    Refactor cluster-nodes component to display GPU information and add badge for not ready nodes

--- src/components/k8s/cluster-nodes.tsx
+++ src/components/k8s/cluster-nodes.tsx
@@ -6,6 +6,7 @@ import { Cpu, Laptop, Server } from "lucide-react";
 import { Suspense } from "react";
 import { Skeleton } from "@/components/ui/skeleton";
 import { type V1Node } from "@kubernetes/client-node";
+import { Badge } from "../ui/badge";
 
 const sleep = (value: number) =>
   new Promise((resolve) => setTimeout(resolve, value));
@@ -17,51 +18,84 @@ async function NodesList() {
     getNodes().then(async (nodes) => {
       await sleep(1000);
       clearTimeout(timeout);
+
+      const ready = nodes
+        .filter((node) =>
+          node.status?.conditions?.some(
+            (c) => c.type === "Ready" && c.reason === "KubeletReady"
+          )
+        )
+        .filter(
+          (node) => node.metadata?.labels!["nvidia.com/gpu.count"] === "1"
+        );
+
+      console.log(ready.length);
+
       resolve(nodes);
     });
   });
 
   return (
     <div className="grid gap-8">
-      {nodes.map((node) => (
-        <div key={node.metadata!.name} className="flex items-center gap-4">
-          <Avatar className="hidden h-9 w-9 sm:flex">
-            {/* <AvatarImage src="/avatars/01.png" alt="Avatar" /> */}
-            <AvatarFallback>
-              {
-                // @ts-ignore
-                node.metadata?.labels["nvidia.com/gpu.product"]?.includes(
-                  "Laptop"
-                ) ? (
-                  <Laptop className="h-5 w-5" />
-                ) : (
-                  <Server className="h-5 w-5" />
-                )
-              }
-            </AvatarFallback>
-          </Avatar>
-          <div className="grid gap-1">
-            <p className="text-sm font-medium leading-none">
-              {node.metadata!.name}
-            </p>
-            <div className="flex items-center gap-1">
-              <Cpu className="h-4 w-4" />
-              <div className="text-sm text-muted-foreground">
-                {node.status?.capacity?.cpu}{" "}
-                {node.status?.nodeInfo?.architecture}
-                {", "}
+      {nodes.map((node) => {
+        const ready = node.status?.conditions?.some(
+          (c) => c.type === "Ready" && c.reason === "KubeletReady"
+        );
+
+        return (
+          <div
+            key={node.metadata?.name}
+            className={
+              ready
+                ? "flex items-center gap-4"
+                : "flex items-center gap-4 text-muted-foreground"
+            }
+          >
+            <Avatar className="hidden h-9 w-9 sm:flex">
+              {/* <AvatarImage src="/avatars/01.png" alt="Avatar" /> */}
+              <AvatarFallback>
                 {
                   // @ts-ignore
-                  node.metadata?.labels["nvidia.com/gpu.product"]!
+                  node.metadata?.labels["nvidia.com/gpu.product"]?.includes(
+                    "Laptop"
+                  ) ? (
+                    <Laptop className="h-5 w-5" />
+                  ) : (
+                    <Server className="h-5 w-5" />
+                  )
                 }
+              </AvatarFallback>
+            </Avatar>
+            <div className="grid gap-1">
+              <p className="text-sm font-medium leading-none">
+                {node.metadata?.name}
+                {!ready && (
+                  <Badge className="mx-2" variant="destructive">
+                    Not Ready
+                  </Badge>
+                )}
+              </p>
+              <div className="flex items-center gap-1">
+                <Cpu className="h-4 w-4" />
+                <div className="text-sm text-muted-foreground">
+                  {node.status?.capacity?.cpu}{" "}
+                  {node.status?.nodeInfo?.architecture}
+                  {", "}
+                  {
+                    // @ts-ignore
+                    node.metadata?.labels["nvidia.com/gpu.product"]
+                  }
+                </div>
               </div>
             </div>
+            <div className="ml-auto font-medium">
+              <TimeAgo
+                date={node.metadata?.creationTimestamp?.toUTCString() as string}
+              />
+            </div>
           </div>
-          <div className="ml-auto font-medium">
-            <TimeAgo date={node.metadata?.creationTimestamp?.toUTCString()!} />
-          </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
