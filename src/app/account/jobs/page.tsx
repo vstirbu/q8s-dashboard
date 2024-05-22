59aa65020c19a6da3a21149a727f8bf11644280d
Vlad Stirbu <vstirbu@gmail.com>
2024-05-22 19:45:24 +0300

    add jobs in last seven days plot

--- src/app/account/jobs/page.tsx
+++ src/app/account/jobs/page.tsx
@@ -1,12 +1,41 @@
 import { auth } from "@/lib/auth";
 import { getJobs } from "@/lib/k8s";
 import JobsList from "@/components/cards/jobs";
+import prisma from "@/lib/db";
+import { JobRuns } from "@/components/job-runs";
 
 export default async function Jobs() {
   const session = await auth();
   const user = session?.user;
 
   const jobs = await getJobs(user?.id!);
+
+  const completed = await prisma.jobs.findMany({
+    where: {
+      ownerId: user?.id,
+    },
+  });
+
+  const data = completed.reduce((acc, job) => {
+    const jobDate = new Date(job.startTime).toLocaleDateString([], {
+      // year: "numeric",
+      month: "short",
+      day: "numeric",
+    });
+
+    if (acc[jobDate]) {
+      acc[jobDate] += 1;
+    } else {
+      acc[jobDate] = 1;
+    }
+
+    return acc;
+  }, arrayOfSevenDaysBeforeToday());
+
+  const transformed = Object.keys(data).map((key) => ({
+    x: key,
+    y: data[key],
+  }));
 
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
@@ -15,6 +44,7 @@ export default async function Jobs() {
       </div>
       {/* <div className="flex gap-4">List of jobs</div> */}
       <JobsList jobs={jobs} />
+      <JobRuns transformed={transformed} />
       {/* <div
             className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
             x-chunk="dashboard-02-chunk-1"
@@ -31,4 +61,20 @@ export default async function Jobs() {
           </div> */}
     </main>
   );
+}
+
+function arrayOfSevenDaysBeforeToday() {
+  const days = Array.from({ length: 7 }, (_, i) => {
+    const date = new Date();
+    date.setDate(date.getDate() - i);
+    return date.toLocaleDateString([], {
+      month: "short",
+      day: "numeric",
+    });
+  });
+
+  return days.reverse().reduce((acc, day) => {
+    acc[day] = 0;
+    return acc;
+  }, {} as Record<string, number>);
 }
