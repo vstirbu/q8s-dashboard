8024514b8f8c154c2b67d4a78aa5b10cc56735b3
Vlad Stirbu <vstirbu@gmail.com>
2024-05-24 20:45:30 +0300

    patch array

--- src/app/api/webhooks/mutate/route.ts
+++ src/app/api/webhooks/mutate/route.ts
@@ -10,22 +10,22 @@ export async function POST(req: Request) {
 
   console.log("username", value);
 
+  const patch = [
+    {
+      op: "add",
+      //   path: "metadata.labels.qubernetes.dev~1user",
+      path: '/metadata/labels/"qubernetes.dev/user"',
+      value,
+    },
+  ];
+
   return Response.json({
     apiVersion: "admission.k8s.io/v1",
     kind: "AdmissionReview",
     response: {
       uid,
       allowed: true,
-      patch: [
-        Buffer.from(
-          JSON.stringify({
-            op: "add",
-            //   path: "metadata.labels.qubernetes.dev~1user",
-            path: 'metadata/labels/"qubernetes.dev/user"',
-            value,
-          })
-        ).toString("base64"),
-      ],
+      patch: Buffer.from(JSON.stringify(patch)).toString("base64"),
       patchType: "JSONPatch",
     },
   });
